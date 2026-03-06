c04ab5f6d915568dcccf303b901ae6f0618bf552
jbisits <jbisits@gmail.com>
2026-03-06 12:35:05 +1100

    March 2026 updates

--- bibs/update_presentations.zsh
+++ bibs/update_presentations.zsh
@@ -1,4 +1,5 @@
 sed -i '' '/abstract/d' presentations.bib
-sed -i '' '/year/a\ 
+sed -i '' '/year/ s/$/,/' presentations.bib
+sed -i '' '/year/a\
    keywords = {presentation},\
 ' presentations.bib
